a8ce6896f84b21392f4182c08d506b859da1455d
Adi <aditya.24@hotmail.com>
2017-05-12 00:20:11 -0700

    templating begun

--- app_server/controllers/blocks.js
+++ app_server/controllers/blocks.js
@@ -1,6 +1,39 @@
 /* GET 'home' page */
 module.exports.blocklist = function(req, res){
-	res.render('blocks-list', { title: 'Home' });
+	res.render('blocks-list', {
+								pageHeader: {
+										title: 'MendWell',
+										strapline: 'Track your medications and wellbeing'
+							    }, 
+							    day: [{
+							    		medications:[
+								    					  {
+								    						medName:'Tylenol',
+								    						medDosage: '3 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Aspirin',
+								    						medDosage: '2 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Buckleys',
+								    						medDosage: '2 spoonfulls/day'
+								    					  }
+							    		],
+							    		bodyStatus:[
+							    					{part: 'Left leg',
+							    					 condition: 3
+							    					},
+							    					{part: 'Right arm',
+							    					 condition: 6
+							    					},
+							    					{part: 'Head',
+							    					 condition: 9
+							    					}
+							    		]
+							    }] 
+							  }
+			   ); 
 };
 
 /* GET 'block info' page */
