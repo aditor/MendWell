93d1712495bb5a1588092a759bd99b1a3cc9a6ad
Adi <aditya.24@hotmail.com>
2017-05-13 15:59:56 -0700

    replicated rows

--- app_server/controllers/blocks.js
+++ app_server/controllers/blocks.js
@@ -5,7 +5,7 @@ module.exports.blocklist = function(req, res){
 										title: 'MendWell',
 										strapline: 'Track your medications and wellbeing'
 							    }, 
-							    day: [{
+							    days: [{date: '05/07/2017',
 							    		medications:[
 								    					  {
 								    						medName:'Tylenol',
@@ -28,6 +28,30 @@ module.exports.blocklist = function(req, res){
 							    					 condition: 6
 							    					},
 							    					{part: 'Head',
+							    					 condition: 9
+							    					}
+							    		]
+							    },
+							    {		date: '05/08/2017',
+							    		medications:[
+								    					  {
+								    						medName:'Adderall',
+								    						medDosage: '5 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Percocet',
+								    						medDosage: '1 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Valium',
+								    						medDosage: '3 spoonfulls/day'
+								    					  }
+							    		],
+							    		bodyStatus:[
+							    					{part: 'Left arm',
+							    					 condition: 4
+							    					},
+							    					{part: 'Stomach',
 							    					 condition: 9
 							    					}
 							    		]
