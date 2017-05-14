84e9cb81e0a2aa02a14193a659974b6bcfd83025
Adi <aditya.24@hotmail.com>
2017-05-14 00:09:41 -0700

    body status changes

--- app_server/controllers/blocks.js
+++ app_server/controllers/blocks.js
@@ -5,7 +5,7 @@ module.exports.blocklist = function(req, res){
 										title: 'MendWell',
 										strapline: 'Track your medications and wellbeing'
 							    }, 
-							    days: [{date: '05/07/2017',
+							    days: [{date: '05/08/2017',
 							    		medications:[
 								    					  {
 								    						medName:'Tylenol',
@@ -32,7 +32,7 @@ module.exports.blocklist = function(req, res){
 							    					}
 							    		]
 							    },
-							    {		date: '05/08/2017',
+							    {		date: '05/07/2017',
 							    		medications:[
 								    					  {
 								    						medName:'Adderall',
@@ -62,7 +62,39 @@ module.exports.blocklist = function(req, res){
 
 /* GET 'block info' page */
 module.exports.blockInfo = function(req, res){
-	res.render('blocks-info', { title: 'Block info' });
+	res.render('blocks-info', {
+								pageHeader: {
+										title: 'MendWell',
+										strapline: 'Track your medications and wellbeing'
+							    }, 
+							    day : {date: '05/08/2017',
+							    		medications:[
+								    					  {
+								    						medName:'Tylenol',
+								    						medDosage: '3 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Aspirin',
+								    						medDosage: '2 pills/day'
+								    					  },
+								    					  {
+								    					  	medName:'Buckleys',
+								    						medDosage: '2 spoonfulls/day'
+								    					  }
+							    		],
+							    		bodyStatus:[
+							    					{part: 'Left leg',
+							    					 condition: 3
+							    					},
+							    					{part: 'Right arm',
+							    					 condition: 6
+							    					},
+							    					{part: 'Head',
+							    					 condition: 9
+							    					}
+							    		]
+							    } 
+							  });
 };
 
 /* GET 'Add block' page */
