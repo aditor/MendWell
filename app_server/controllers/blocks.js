3d5ad837acc3d887977c05cc632ecc7f3ca321a9
Adi <aditya.24@hotmail.com>
2017-05-15 23:10:42 -0700

    set up database

--- app_server/controllers/blocks.js
+++ app_server/controllers/blocks.js
@@ -1,3 +1,24 @@
+var mongoose = require( 'mongoose' );
+
+
+// models
+var medication = new mongoose.Schema({
+	medName: {type: String, required: true},
+	medDosage: {type: String, required: true}
+})
+var bodyPart = new mongoose.Schema({
+	part: {type: String, required: true},
+	condition: {type:Number, "default": 0}
+})
+var daysSchema = new mongoose.Schema({
+	date: String,
+	medList: [medication],
+	bodyStatus: [bodyPart]
+})
+
+mongoose.model('Day', daysSchema);
+
+
 /* GET 'home' page */
 module.exports.blocklist = function(req, res){
 	res.render('blocks-list', {
