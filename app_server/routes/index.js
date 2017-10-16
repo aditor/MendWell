0ef566850539becb180f64c6360fb98c2a4632cb
Adi <aditya.24@hotmail.com>
2017-10-16 00:01:01 -0700

    UI changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,7 +6,8 @@ var ctrlOthers = require('../controllers/others');
 /* GET home page. */
 router.get('/', ctrlConditions.conditionlist);
 router.get('/condition/:conditionid', ctrlConditions.conditionInfo);
-router.get('/addACondition', ctrlConditions.addCondition);
+router.get('/addCondition', ctrlConditions.addCondition);
+//router.post('/addCondition', ctrlConditions.doAddCondition);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
